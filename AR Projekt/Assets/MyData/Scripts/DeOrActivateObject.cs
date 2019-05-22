6dd43e1194db81a7394a7464b7862d3e5f9dab55
Klibink <35121031+Klibink@users.noreply.github.com>
2019-05-22 12:55:23 +0200

    added Project to Repository

---
+++ AR Projekt/Assets/MyData/Scripts/DeOrActivateObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeOrActivateObject : MonoBehaviour
+{
+    public bool isActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            this.gameObject.SetActive(true);
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void SetActive()
+    {
+        isActive = !isActive;
+        Debug.Log("Test");
+    }
+}
