db7fb6e8644e454aee4be064056238b63f20eac5
Klibink <35121031+Klibink@users.noreply.github.com>
2019-05-31 13:14:58 +0200

    added PocketDroids Tutorial Stuff

---
+++ AR Projekt/Assets/MyData/PocketDroidsStuff/Models/Droids/Droid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Droid : MonoBehaviour
+{
+    [SerializeField] private float spawnRate = 0.10f;
+    [SerializeField] private float catchRate = 0.10f;
+    [SerializeField] private int attack = 0;
+    [SerializeField] private int defense = 0;
+    [SerializeField] private int hp = 0;
+
+    public float SpawnRate { get => spawnRate; }
+    public float CatchRate { get => catchRate;  }
+    public int Attack { get => attack;  }
+    public int Defense { get => defense;  }
+    public int Hp { get => hp;  }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
---
+++ AR Projekt/Assets/MyData/PocketDroidsStuff/Scenes/World/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int xp = 0;
+    [SerializeField] private int requiredXp = 0;
+    [SerializeField] private int levelBase = 100;
+    [SerializeField] private List<GameObject> droids = new List<GameObject>();
+    private int lvl = 1;
+
+    public int Xp {
+        get => xp;
+    }
+    public int RequiredXp {
+        get => requiredXp;
+    }
+    public int LevelBase {
+        get => levelBase;
+    }
+    public List<GameObject> Droids {
+        get => droids;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitLevelData();
+    }
+
+    public void AddXp(int xp)
+    {
+        this.xp = Mathf.Max(0, xp);
+    }
+
+    public void AddDroid(GameObject droid)
+    {
+        droids.Add(droid);
+    }
+
+    private void InitLevelData()
+    {
+        lvl = (xp / levelBase) + 1;
+        requiredXp = levelBase * lvl;
+    }
+   
+}
