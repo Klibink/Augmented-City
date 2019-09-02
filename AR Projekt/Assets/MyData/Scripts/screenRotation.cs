dd15a175b3c94d6a5ba9bb1ceb259c961c1919a1
lruehle <35702288+lruehle@users.noreply.github.com>
2019-09-02 13:48:25 +0200

    auto rotate Screen to Landscape
    Portrait mode disabled for this scene. muss in folgender szene evtl. wieder enabled werden über
    https://docs.unity3d.com/ScriptReference/ScreenOrientation.AutoRotation.html

---
+++ AR Projekt/Assets/MyData/Scripts/screenRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+        Screen.orientation = ScreenOrientation.AutoRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
