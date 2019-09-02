a7059fc26c1198c6138efda9d6e2984b9abdc9ad
lruehle <35702288+lruehle@users.noreply.github.com>
2019-09-02 15:01:15 +0200

    rotation + text Align
    text Align should still be tested on mobile.
    rotation now autorotate to landscape when scene starts

--- AR Projekt/Assets/MyData/Scripts/screenRotation.cs
+++ AR Projekt/Assets/MyData/Scripts/screenRotation.cs
@@ -7,6 +7,7 @@ public class screenRotation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Screen.orientation = ScreenOrientation.LandscapeRight;
         Screen.autorotateToLandscapeLeft = true;
         Screen.autorotateToLandscapeRight = true;
         Screen.autorotateToPortrait = false;
