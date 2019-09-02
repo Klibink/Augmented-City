742c20e4c5764f936693a71bde9c969cbf19f1e3
lruehle <35702288+lruehle@users.noreply.github.com>
2019-09-02 12:39:51 +0200

    PuzzleGameScene Added
    Assets haben nun in /myData/scenes die PuzzleGame Szene ; Skripte sind unter /MyData/scripts ; Sprites sind unter /myData/puzzleparts
    
    Position des UI texts hab ich bei Display 16:9 getestet, ist für die Auflösung meines Handys gut gegangen

---
+++ AR Projekt/Assets/MyData/Scripts/PuzzleGameControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleGameControl : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject textSmall;
+    public GameObject Quader;
+    public GameObject pieces;
+    public GameObject places;
+    public Text textfield;
+    private int timer = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        textSmall.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer --;
+        if(timer<=0)
+        {
+            pieces.SetActive(true);
+            Quader.SetActive(true);
+            /*for (int i= 0; i<=places.transform.childCount;i++)
+            {
+               places.gameObject.transform.GetChild(i).gameObject.SetActive(true);
+            }*/
+            places.SetActive(true);
+        }
+        if (puzzleScript.locked&&puzzle2Script.locked&&puzzle3Script.locked&&puzzle4Script.locked&&puzzle5Script.locked)
+        {
+            finished();
+        }
+    }
+
+    void finished()
+    {
+        textfield.text = "Interesting! Looks like those are coordinates. K, where do they lead to?";
+    }
+}
