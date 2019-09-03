db43ad1ca46202744515bab09d82527abec2ed14
lruehle <35702288+lruehle@users.noreply.github.com>
2019-09-03 18:03:12 +0200

    StartScreen & VideoPlayer
    Start Screen added
    Video Plays & triggers next scene

---
+++ AR Projekt/Assets/MyData/Scripts/VideoLoadScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoLoadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public VideoPlayer Vp;
+    void Start()
+    {
+        Vp.loopPointReached += EndReached;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void EndReached(VideoPlayer vp)
+    {
+        Debug.Log("loadScene");
+        SceneManager.LoadScene("StartScene");
+    }
+}
---
+++ AR Projekt/Assets/MyData/Scripts/startSceneScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class startSceneScript : MonoBehaviour
+{
+
+    private int timer = 100;
+    public Text text;
+    public GameObject btn;
+    public GameObject canvas;
+    public GameObject vPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Debug.Log(timer);
+        if (timer <= 50)
+        {
+            text.text = "Mörder To GO";
+        }
+        if(timer <= 0)
+        {
+            btn.SetActive(true);
+        }
+        else
+        {
+            timer--;
+        }
+    }
+
+    public void playVid()
+    {
+        vPlayer.SetActive(true);
+        btn.SetActive(false);
+        canvas.SetActive(false);
+        
+    }
+
+}
